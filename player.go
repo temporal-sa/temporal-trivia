2e39286e58e9f549ba7630c446813e78b2c8b30c
cici thomson <cici.thomson@temporal.io>
2023-06-29 14:54:27 -0400

    Initial checkin

---
+++ player.go
@@ -0,0 +1,86 @@
+package triviagame
+
+import (
+	"errors"
+	"os"
+
+	activities "github.com/ktenzer/temporal-trivia/activities"
+	"github.com/ktenzer/temporal-trivia/resources"
+	_ "go.temporal.io/sdk/contrib/tools/workflowcheck/determinism"
+	"go.temporal.io/sdk/workflow"
+)
+
+// Add player workflow definition
+func AddPlayerWorkflow(ctx workflow.Context, workflowInput resources.AddPlayerWorkflowInput) error {
+	logger := workflow.GetLogger(ctx)
+	logger.Info("Trivia Game Started")
+
+	// Activity options
+	ctx = workflow.WithLocalActivityOptions(ctx, setDefaultLocalActivityOptions())
+
+	activityInput := resources.QueryPlayerActivityInput{
+		WorkflowId: workflowInput.GameWorkflowId,
+		Player:     workflowInput.Player,
+		QueryType:  "normal",
+	}
+
+	// run activity to start game and pre-fetch trivia questions and answers
+	var isPlayer bool
+	err := workflow.ExecuteLocalActivity(ctx, activities.QueryPlayerActivity, activityInput).Get(ctx, &isPlayer)
+	if err != nil {
+		logger.Error("Query Player Activity failed.", "Error", err)
+		return err
+	}
+
+	if isPlayer {
+		return errors.New("Player " + workflowInput.Player + " already exists!")
+	}
+
+	// run activity to check for valid name
+	moderationInput := resources.ModerationInput{
+		Url:  os.Getenv("MODERATION_URL"),
+		Name: workflowInput.Player,
+	}
+
+	// run moderation activity to check provided name
+	var moderationResult bool
+	modErr := workflow.ExecuteActivity(ctx, activities.ModerationActivity, moderationInput).Get(ctx, &moderationResult)
+	if modErr != nil {
+		logger.Error("Moderation Activity failed.", "Error", modErr)
+		return modErr
+	}
+
+	if !moderationResult {
+		return errors.New("Player name is invalid")
+	}
+
+	// Add player via signal
+	addPlayerSignal := PlayerSignal{
+		Action: "Player",
+		Player: workflowInput.Player,
+	}
+
+	err = workflow.SignalExternalWorkflow(ctx, workflowInput.GameWorkflowId, "", "add-player-signal", addPlayerSignal).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// Ensure player is added
+	activityInput = resources.QueryPlayerActivityInput{
+		WorkflowId: workflowInput.GameWorkflowId,
+		Player:     workflowInput.Player,
+		QueryType:  "poll",
+	}
+
+	err = workflow.ExecuteLocalActivity(ctx, activities.QueryPlayerActivity, activityInput).Get(ctx, &isPlayer)
+	if err != nil {
+		logger.Error("Query Player Activity failed before adding to game.", "Error", err)
+		return err
+	}
+
+	if !isPlayer {
+		return errors.New("Player " + workflowInput.Player + " could not be added to the game")
+	}
+
+	return nil
+}
---
+++ struct.go
@@ -0,0 +1,59 @@
+package resources
+
+type GameWorkflowConfig struct {
+	ChatGptKey string `json:"chatGptKey"`
+}
+
+type GameWorkflowInput struct {
+	Category          string `json:"category"`
+	NumberOfQuestions int    `json:"numberOfQuestions"`
+	NumberOfPlayers   int    `json:"numberOfPlayers"`
+	AnswerTimeLimit   int    `json:"answerTimeLimit"`
+	StartTimeLimit    int    `json:"startTimeLimit"`
+	ResultTimeLimit   int    `json:"resultTimeLimit"`
+}
+
+type AddPlayerWorkflowInput struct {
+	GameWorkflowId string `json:"gameWorkflowId"`
+	Player         string `json:"player"`
+}
+
+type TriviaQuestionsActivityInput struct {
+	Key               string `json:"key"`
+	Category          string `json:"category"`
+	NumberOfQuestions int    `json:"question"`
+}
+
+type QueryPlayerActivityInput struct {
+	WorkflowId string `json:"workflowId"`
+	Player     string `json:"player"`
+	QueryType  string `json:"queryType"`
+}
+
+type Result struct {
+	Question          string                `json:"question"`
+	Answer            string                `json:"answer"`
+	Submissions       map[string]Submission `json:"submissions"`
+	MultipleChoiceMap map[string]string     `json:"multipleChoiceAnswers"`
+	Winner            string                `json:"winner"`
+}
+
+type Submission struct {
+	Answer    string `json:"answer"`
+	IsCorrect bool   `json:"isCorrect"`
+	IsFirst   bool   `json:"isFirst"`
+}
+
+type ScoreBoard struct {
+	Player string `json:"value"`
+	Score  int    `json:"key"`
+}
+
+type Player struct {
+	Score int `json:"score"`
+}
+
+type ModerationInput struct {
+	Url  string `json:"url"`
+	Name string `json:"name"`
+}
